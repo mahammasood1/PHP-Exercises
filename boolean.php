40807fdb9dbbaef2608f088b0f07ea976e84e5a3
mahammasood1 <mahammasood31@gmail.com>
2019-06-21 12:32:54 +0500

    Add files via upload

---
+++ boolean.php
@@ -0,0 +1,12 @@
+<?php
+
+$task = [
+    'title' => 'complete notes',
+    'due' => 'today',
+    'assigned_to' => 'Maham',
+    'completed' => true
+];
+
+
+
+require 'boolean.view.php';
---
+++ boolean.view.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+
+<header>
+ 
+    <h1>Arrays in php</h1>
+
+</header>
+
+<body>
+
+    <ul>
+           <?= $task['completed'] ? 'Done':'Not done' ?>
+
+    </ul>
+
+
+</body>
+</html>
