5131b009ebfebfbc131ac58853dc85d95029ea78
mahammasood1 <mahammasood31@gmail.com>
2019-06-24 17:45:17 +0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -11,4 +11,30 @@ function checkAge($age)
 
 }
 
+function connectToDb()
+{
+try{
+ return new pdo('mysql:host=127.0.0.1;dbname=mytodo','root', '');
+}
+
+catch(PDOexception $e)
+{
+    die($e->getMessage());
+}
+
+}
+
+function fetchAllTasks($pdo)
+{
+    $stmt = $pdo->prepare('select * from todos');
+
+    $stmt->execute();
+
+    return $stmt->fetchAll(PDO::FETCH_CLASS, 'Task');
+//var_dump($results[0]->description);
+
+//var_dump($tasks[0]->foobar());
+
+}
+
 
